8f60b8b7415b5e8e80765085461f0fa6cb6322b1
kevinnika1 <87488963+kevinnika1@users.noreply.github.com>
2021-09-24 15:06:09 +0100

    Create trivyjson.py

---
+++ trivyjson.py
@@ -0,0 +1,34 @@
+import json
+
+try:
+
+    with open('trivy-results.json') as json_file:
+        data = json.load(json_file)
+        
+        
+        path=data[0]['Target']
+        result= []
+        if ("Vulnerabilities" in data[0]):
+
+            for v in data[0]['Vulnerabilities']:
+                thisdict= {}
+                thisdict['path']=path
+                thisdict['message']=str("Package Name: "+v.get('PkgName')+", Installed version: "+v.get('InstalledVersion')+", Fixed Version: "+v.get('FixedVersion')+"  URL for more info: "+v.get('PrimaryURL'))
+                if (v['Severity']=="UNKNOWN" or v['Severity']=="MEDIUM" or v['Severity']=="LOW"):
+                    thisdict['annotation_level']="notice"
+                elif (v['Severity']=="HIGH"):
+                    thisdict['annotation_level']="warning"
+                elif (v['Severity']=="CRITICAL"):
+                    thisdict['annotation_level']="failure"
+                thisdict['line']=0
+                if ("Title" in v):
+                    thisdict['title']=str(v.get('VulnerabilityID')+", "+v.get('Title')+", "+"Severity: "+v.get('Severity'))
+                else:
+                    thisdict['title']=str(v.get('VulnerabilityID')+", Severity: "+v.get('Severity'))
+                result.append(thisdict)
+
+    with open("trivyparsedresults.json", "w") as outfile:
+        json.dump(result, outfile, indent=4)
+
+except IOError:
+    print("File not accessible")
